5f622121bc3003ef57365845de56545f48efd67e
angelagongli <angelagongli@gmail.com>
2020-11-12 11:24:56 -0600

    add Coach.cs model

---
+++ LLApp/LLDB/Models/Coach.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LLDB.Models
+{
+    class Coach
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string EmailAddress { get; set; }
+    }
+}
