c556e81074e9e4897f8e4159d35f530423eb3771
lw3b3r <daevahush@gmail.com>
2020-11-12 16:49:08 -0500

    Get parents working

--- FrontEnd/AllParents.js
+++ FrontEnd/AllParents.js
@@ -1,23 +1,32 @@
-function GetAllParents() {
 
+function GetAllParents() {
+    
     fetch('https://localhost:44331/parent/get/')
     .then(response => response.json())
     .then(result => {
         document.querySelectorAll('#parents tbody tr').forEach(element => element.remove());
         let table = document.querySelector('#parents tbody');
 
-        for (let i = 0; i < result.length; i++) {
+        for(let i = 0; i < result.length; ++i) {
             let row = table.insertRow(table.rows.length);
 
-            let pnCell = row.insertCell(0);
-            pnCell.innerHTML = result[i].Name;
+            let nameCell = row.insertCell(0);
+            nameCell.innerHTML = result[i].name;
 
-            let pCell = row.insertCell(1);
-            pCell.innerHTML = result[i].PhoneNumber;
+            let phoneCell = row.insertCell(1);
+            phoneCell.innerHTML = result[i].phoneNumber;
 
-            let eCell = row.insertCell(2);
-            eCell.innerHTML = result[i].EmailAddress;
+            let emailCell = row.insertCell(2);
+            emailCell.innerHTML = result[i].emailAddress;
+
+            let addCell = row.insertCell(5);
+            let btn = document.createElement('input');
+            btn.type = 'button';
+            btn.id = result[i].id;
+            btn.value = 'Add to Cart';
+
             
+        
         }
     });
 }
