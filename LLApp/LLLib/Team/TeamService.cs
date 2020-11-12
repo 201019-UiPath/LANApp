8c2cab31fb1ff79b24e2d33dbaf68e2273184df4
lw3b3r <daevahush@gmail.com>
2020-11-12 11:05:00 -0500

    Added services for all models

--- LLApp/LLLib/Team/TeamService.cs
+++ LLApp/LLLib/Team/TeamService.cs
@@ -2,6 +2,27 @@ namespace LLLib
 {
     public class TeamService
     {
-        
+        private ITeamRepo repo;
+
+        public ParentService(ITeamRepo repo) {
+            this.repo = repo;
+        }
+
+        public void AddTeam(Team team) {
+            repo.AddTeam(team);
+        }
+
+        public void UpdateTeam(Team team) {
+             repo.UpdateTeam(team);
+         }
+
+         public void DeleteTeam(Team team) {
+             repo.DeleteTeam(team);
+         }
+
+         public List<Team> GetAllTeams() {
+             List<Team> team = repo.GetAllTeams();
+             return team;
+         }
     }
 }
